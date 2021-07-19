0c0d5d00f482742024bc2a98b37b4d0213efd322
John Frankland <john.frankland@ganil.fr>
2021-07-19 10:39:38 +0200

    master branch is not officially C++11-only yet - try to keep back. compat.

--- KVIndra/calibration/KVLightEnergyCsI_Tabacaru_Camp2_10_17.cpp
+++ KVIndra/calibration/KVLightEnergyCsI_Tabacaru_Camp2_10_17.cpp
@@ -11,11 +11,11 @@ Double_t KVLightEnergyCsI_Tabacaru_Camp2_10_17::tabacaru(double* L, double* par)
    // L=Lumiere
    // par = a1, a2, a3, a4
 
-   auto hl = L[0];
-   auto a1t = par[0];
-   auto a2t = par[1];
-   auto a3t = par[2];
-   auto a4t = par[3];
+   double hl = L[0];
+   double a1t = par[0];
+   double a2t = par[1];
+   double a3t = par[2];
+   double a4t = par[3];
 
    double x;
    if (a2t < 1.e-7)
@@ -23,26 +23,26 @@ Double_t KVLightEnergyCsI_Tabacaru_Camp2_10_17::tabacaru(double* L, double* par)
    else
       x = 2000.;
 
-   auto az2 = A * Z * Z;
-   auto argx = 1. + x / (a2t * az2);
-   auto cond = true;
+   double az2 = A * Z * Z;
+   double argx = 1. + x / (a2t * az2);
+   double cond = true;
 
-   auto niter = 0;
-   auto eps = 1.e-4;
-   auto fbe = 1.0;
+   double niter = 0;
+   double eps = 1.e-4;
+   double fbe = 1.0;
 
-   auto   E0 = a3t * A;
+   double   E0 = a3t * A;
 
    while (cond) {
       niter = niter + 1;
 
-      auto a4e = a4t;
+      double a4e = a4t;
       if (x < E0) a4e = 0;
 
-      auto fx = -hl + fbe * a1t * (x - a2t * az2 * log(argx) + a2t * a4e * az2 * log((x + a2t * az2) / (E0 + a2t * az2)));
-      auto fpx = fbe * a1t * (x + a2t * az2 * a4e) / (x + a2t * az2);
+      double fx = -hl + fbe * a1t * (x - a2t * az2 * log(argx) + a2t * a4e * az2 * log((x + a2t * az2) / (E0 + a2t * az2)));
+      double fpx = fbe * a1t * (x + a2t * az2 * a4e) / (x + a2t * az2);
 
-      auto dx = - fx / fpx;
+      double dx = - fx / fpx;
       x += dx;
       argx = 1. + x / (a2t * az2);
       cond = (abs(dx / x) >= eps) && (argx > 0.) && (niter <= 50);
