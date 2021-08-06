26ac71ad6efe668517afc7f4ff117176a4e3a14d
John Frankland <john.frankland@ganil.fr>
2021-08-06 11:02:23 +0200

    add indrafazia directory to doxygen, now there are classes in it

--- doc/doxygen/rootlogon.C
+++ doc/doxygen/rootlogon.C
@@ -2,8 +2,7 @@
    gROOT->ProcessLine(".L DocConverter.cxx+");
    ConvertAllModules("kaliveda.git/KVMultiDet");
    ConvertAllModules("kaliveda.git/KVIndra");
-   ConvertAllModules("kaliveda.git/VAMOS");
    ConvertAllModules("kaliveda.git/FAZIA");
+   ConvertAllModules("kaliveda.git/indrafazia");
    ConvertAllModules("kaliveda.git/MicroStat");
-   ConvertAllModules("kaliveda.git/BackTrack");
 }
